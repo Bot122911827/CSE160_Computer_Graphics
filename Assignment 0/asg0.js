710eb2a9730029a69c25141961747a2f7aaa7751
Bot122911827 <nkghost122911827@gmail.com>
2022-09-29 22:27:12 -0700

    Update

---
+++ Assignment 0/asg0.js
@@ -0,0 +1,51 @@
+// DrawTriangle.js (c) 2012 matsuda
+function main() {  
+  // Retrieve <canvas> element
+  canvas = document.getElementById('example');  
+  if (!canvas) { 
+    console.log('Failed to retrieve the <canvas> element');
+    return false; 
+  } 
+
+  // Get the rendering context for 2DCG
+  ctx = canvas.getContext('2d');
+
+  // Draw a blue rectangle
+  ctx.fillStyle = 'rgba(0, 0, 0, 1.0)'; // Set color to blue
+  ctx.fillRect(0, 0, canvas.width, canvas.height);        // Fill a rectangle with the color
+
+  //instantiate a vector v1
+  v1 = new Vector3(2.25, 2.25, 0);
+  console.log(v1.x);
+
+  drawVector(v1, "red");
+}
+
+//number 2
+function drawVector(vector, color){
+
+  ctx.strokeStyle = color;
+
+  let cx = canvas.width/2;
+  let cy = canvas.height/2;
+  ctx.beginPath();
+  ctx.moveTo(cx, cy);
+  ctx.lineTo(cx + 20*vector.x, cy - 20*vector.y);
+  ctx.stroke(); 
+}
+
+//number 3
+function handleDrawEvent(){
+  let v1 = document.getElementById("name").value;
+  console.log(v1);
+
+  ctx.strokeStyle = 'red';
+
+  let cx = canvas.width/2;
+  let cy = canvas.height/2;
+  ctx.beginPath();
+  ctx.moveTo(cx, cy);
+  ctx.lineTo(cx + 75, cy + 50);
+  ctx.stroke();
+
+}
---
+++ Assignment 0/lib/test.js
@@ -0,0 +1,11 @@
+function main(){
+    console.log("Hello World!")
+
+    alert("WTF")
+
+    let text = prompt("prompt")
+
+    let press = confirm("confirm");
+
+    console.log(text);
+}
